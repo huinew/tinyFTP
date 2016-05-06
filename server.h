8359db2e7903c2995bd41c76235e3f251eaca56b
huinew <74509324@qq.com>
2016-05-06 09:49:33 +0800

    Update server.h

--- server.h
+++ server.h
@@ -1,5 +1,32 @@
 #include <stdio.h>
+
 static const char *default_usernames[] = 
 {
   "ftp","anonymous","public","anon","test","foo"
 };
+
+typedef struct State
+{
+  /* Connection mode: NORMAL, SERVER, CLIENT */
+  int mode;
+
+  /* Is user loggd in? */
+  int logged_in;
+
+  /* Is this username allowed? */
+  int username_ok;
+  char *username;
+  
+  /* Response message to client e.g. 220 Welcome */
+  char *message;
+
+  /* Commander connection */
+  int connection;
+
+  /* Socket for passive connection (must be accepted later) */
+  int sock_pasv;
+
+  /* Transfer process id */
+  int tr_pid;
+
+} State;
