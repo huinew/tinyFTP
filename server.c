17ac6b68bbeb77d8d7bfa18e15e54c8248d36b32
huinew <74509324@qq.com>
2016-05-09 10:25:08 +0800

    Update server.c

--- server.c
+++ server.c
@@ -42,7 +42,6 @@ void serve(int sockfd)
 	{
 		if ((clfd = accept(sockfd, NULL, NULL)) < 0)
 		{
-			syslog(LOG_ERR, "ruptime: accept log err: %s.", strerror(errno));
 			return -1;
 		}
 		
@@ -50,14 +49,7 @@ void serve(int sockfd)
 		{}
 		else if ( pid == 0 )
 		{
-			if ((dup2(clfd, STDOUT_FILENO) != STDOUT_FILENO)
-			|| (dup2(clfd, STDERR_FILENO) != STDERR_FILENO))
-			{
-				syslog(LOG_ERR, "ruptimed: unexpected error");
-			}
-			close(clfd);
-			execl ("/usr/bin/uptime", "uptime", (char *)0);
-			syslog(LOG_ERR, "ruptime: accept log err: %s.", strerror(errno));
+			
 		}
 		else
 		{
@@ -75,38 +67,7 @@ int main(int argc, char* argv[])
 	int sockfd = 0;
 	int err = 0; 
 	int host_name_len = 0;
-	char *host = NULL;
-	
-	if (argc != 1)
-	{
-		printf("usage: ruptime.\n");
-	}
-	if ((n = sysconf(_SC_HOST_NAME_MAX)) < 0)
-	{
-		n = HOST_NAME_MAX;
-	}
-	if ((host = malloc(host_name_len) == NULL))
-	{
-		printf("malloc error.\n");
-		return -1;
-	}
-	if (gethostname(host, host_name_len) < 0)
-	{
-		printf("get host name error.\n");
-		return -1;
-	}
-	daemonize("ruptimed");
-	memset(&hint, 0, sizeof(hint));
-	hint.ai_flags = AI_CANONNAME;
-	hint.ai_socktype = SOCK_STREAM;
-	hint.ai_canonname = NULL;
-	hint.ai_addr = NULL;
-	hint.ad_next = NULL;
-	if ((err = getaddrinfo(host, "ruptime", &hint, &ailist)) != 0)
-	{
-		syslog(LOG_ERR, "ruptime: accept log err: %s.", get_addr_info_err(err));
-		return -1;
-	}
+
 	for (aip = ailist; aip != NULL; aip = aip->ai_next)
 	{
 		if ((sockfd = init_server(SOCK_STREAM, aip->ai_addr,
