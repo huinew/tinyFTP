8ec5d4565742565292b29d2cc0c495daff54c4cf
huinew <74509324@qq.com>
2016-07-13 23:04:05 +0800

    Update client.h

--- client.h
+++ client.h
@@ -24,3 +24,42 @@ static const char *cmdlist_str[] =
   "PORT", "PWD", "QUIT", "RETR", "RMD", "RNFR", "RNTO", "SITE", "SIZE",
   "STOR", "TYPE", "USER", "NOOP" 
 };
+
+#define MAX_CMD_NUM 7
+
+//支持的FTP客户端命令
+enum
+{
+	CMD_GET = 1，
+	CMD_PUT，
+	CMD_MGET，
+	CMD_MPUT，
+	CMD_LS，
+	CMD_CD
+}
+
+
+//用户命令信息
+typedef struct _command_info
+{
+	int cmd_id;		//id that represent command
+	int path_num;
+	char **path;
+}command_info;
+
+typedef struct _command_list
+{
+	char* key;
+	int id;
+}command_list;
+
+static command_list cmd_list[MAX_CMD_NUM] = 
+{
+	{"get", CMD_GET},
+	{"put", CMD_PUT},
+	{"mget", CMD_MGET},
+	{"mput", CMD_MPUT},
+	{"ls", CMD_LS},
+	{"cd", CMD_CD},
+	{"", -1}
+};
